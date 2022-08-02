1c0c4a82923db23a10bcc6feed50f9aab7a08b1b
MIHIGO Yves <ymihigo70@gmail.com>
2022-08-02 11:09:52 +0200

    initial

---
+++ OnlinePaymentWithPayPal/src/main/java/com/payment/main/OnlinePaymentWithPayPalApplication.java
@@ -0,0 +1,13 @@
+package com.payment.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePaymentWithPayPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePaymentWithPayPalApplication.class, args);
+	}
+
+}
---
+++ OnlinePaymentWithPayPal/src/test/java/com/payment/main/OnlinePaymentWithPayPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePaymentWithPayPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
